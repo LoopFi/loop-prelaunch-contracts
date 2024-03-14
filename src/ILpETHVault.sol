02b775653fa1b0a69a133466800d07bdabffac17
0xd4n1el <0xd4n1el@gmail.com>
2024-03-14 22:17:23 +0100

    feat: add claimAndStake

--- src/ILpETHVault.sol
+++ src/ILpETHVault.sol
@@ -3,6 +3,6 @@ pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface ILpETH is IERC20 {
-    function deposit(address receiver) external payable returns (uint256);
+interface ILpETHVault is IERC20 {
+    function stake(uint256 amount, address receiver) external returns (uint256);
 }
--- src/PrelaunchPoints.sol
+++ src/PrelaunchPoints.sol
@@ -4,7 +4,8 @@ pragma solidity 0.8.20;
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
-import { ILpETH, IERC20 } from "./ILpETH.sol";
+import {ILpETH, IERC20} from "./ILpETH.sol";
+import {ILpETHVault} from "./ILpETHVault.sol";
 
 /**
  * @title   PrelaunchPoints
@@ -16,7 +17,12 @@ contract PrelaunchPoints {
     using SafeERC20 for IERC20;
     using SafeERC20 for ILpETH;
 
+    /*//////////////////////////////////////////////////////////////
+                                STORAGE
+    //////////////////////////////////////////////////////////////*/
+
     ILpETH public lpETH;
+    ILpETHVault public lpETHVault;
 
     address public owner;
 
@@ -29,26 +35,47 @@ contract PrelaunchPoints {
 
     mapping(address => uint256) public balances;
 
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
     event Staked(address indexed user, uint256 amount, bytes32 indexed referral);
+    event StakedVault(address indexed user, uint256 amount);
     event Converted(uint256 amountETH, uint256 amountlpETH);
     event Withdrawn(address indexed user, uint256 amount);
     event Claimed(address indexed user, uint256 reward);
     event Recovered(address token, uint256 amount);
     event OwnerUpdated(address newOwner);
-    event LoopAddressUpdated(address newAddress);
+    event LoopAddressesUpdated(address loopAddress, address vaultAddress);
 
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
 
-    /**
-     * @dev Initializes variables, approves lit and sets target dates.
-     */
+    error NothingToClaim();
+    error CannotStakeZero();
+    error CannotWithdrawZero();
+    error FailedToSendEther();
+    error LoopNotActivated();
+    error NotValidToken();
+    error NotAuthorized();
+    error CurrentlyNotPossible();
+    error NoLongerPossible();
+
+    /*//////////////////////////////////////////////////////////////
+                             INITIALIZATION
+    //////////////////////////////////////////////////////////////*/
+
     constructor() {
         owner = msg.sender;
         loopActivation = uint32(block.timestamp + 120 days);
-        startClaimDate = 4294967295;
+        startClaimDate = 4294967295; // Max uint32 ~ year 2107
         TIMELOCK = 7 days;
     }
 
-    // ----- Stake Functions ----- //
+    /*//////////////////////////////////////////////////////////////
+                            STAKE FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
     /**
      * @notice Stakes ETH
@@ -67,13 +94,19 @@ contract PrelaunchPoints {
     }
 
     /**
-     * @dev Generic internal staking function that updates rewards based on previous balances, then update balances
+     * @dev Generic internal staking function that updates rewards based on
+     *      previous balances, then update balances
      * @param _amount    Units to add to the users balance
      * @param _receiver  Address of user who will receive the stake
      * @param _referral  Address of the referral user
      */
-    function _processStake(uint256 _amount, address _receiver, bytes32 _referral) internal onlyBeforeDate(loopActivation){
-        require(_amount > 0, "Cannot stake 0");
+    function _processStake(uint256 _amount, address _receiver, bytes32 _referral)
+        internal
+        onlyBeforeDate(loopActivation)
+    {
+        if (_amount == 0) {
+            revert CannotStakeZero();
+        }
 
         // update storage variables
         totalSupply = totalSupply + _amount;
@@ -82,57 +115,82 @@ contract PrelaunchPoints {
         emit Staked(_receiver, _amount, _referral);
     }
 
-
-
-    // ----- Claim and Widthdraw Functions ----- //
+    /*//////////////////////////////////////////////////////////////
+                        CLAIM AND WITHDRAW FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
     /**
      * @dev Called by a staker to get their vested lpETH
      */
     function claim() external onlyAfterDate(startClaimDate) {
-        uint256 userStake = balances[msg.sender];
-        require(userStake > 0, "Nothing to claim");
-
-        uint256 unclaimedAmount = userStake.mulDiv(totalLpETH, totalSupply);
-        balances[msg.sender] = 0;
-        lpETH.safeTransfer(msg.sender, unclaimedAmount);
-
-        emit Claimed(msg.sender, unclaimedAmount);
+        _claim(msg.sender);
     }
 
+    /**
+     * @dev Called by a staker to get their vested lpETH staked again in a
+     *      Loop vault for extra rewards
+     */
+    function claimAndStake() external onlyAfterDate(startClaimDate) {
+        uint256 claimedAmount = _claim(address(this));
+        lpETH.approve(address(lpETHVault), claimedAmount);
+        lpETHVault.stake(claimedAmount, msg.sender);
+
+        emit StakedVault(msg.sender, claimedAmount);
+    }
+
+    function _claim(address _receiver) internal returns (uint256 claimedAmount) {
+        uint256 userStake = balances[msg.sender];
+        if (userStake == 0) {
+            revert NothingToClaim();
+        }
+
+        claimedAmount = userStake.mulDiv(totalLpETH, totalSupply);
+        balances[msg.sender] = 0;
+        lpETH.safeTransfer(_receiver, claimedAmount);
+
+        emit Claimed(msg.sender, claimedAmount);
+    }
 
     /**
      * @dev Called by a staker to withdraw all their ETH
-     * Note Can only be called after the loop address is set and before claiming lpETH, 
+     * Note Can only be called after the loop address is set and before claiming lpETH,
      * i.e. for at least TIMELOCK
      */
-    function withdraw() external onlyAfterDate(loopActivation) onlyBeforeDate(startClaimDate){
+    function withdraw() external onlyAfterDate(loopActivation) onlyBeforeDate(startClaimDate) {
         uint256 userStake = balances[msg.sender];
-        require(userStake > 0, "Cannot withdraw 0");
+        if (userStake == 0) {
+            revert CannotWithdrawZero();
+        }
 
         totalSupply = totalSupply - userStake;
         balances[msg.sender] = 0;
 
-        (bool sent, ) = msg.sender.call{value: userStake}("");
-        require(sent, "Failed to send Ether");
+        (bool sent,) = msg.sender.call{value: userStake}("");
+        if (!sent) {
+            revert FailedToSendEther();
+        }
 
         emit Withdrawn(msg.sender, userStake);
     }
 
-    // ----- Protected Functions ----- //
+    /*//////////////////////////////////////////////////////////////
+                            PROTECTED FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
     /**
      * @dev Called by a owner to convert all the staked ETH to get lpETH
      */
     function convertAll() external onlyAuthorized {
-        require(block.timestamp - loopActivation > TIMELOCK, "Not activated");
+        if (block.timestamp - loopActivation <= TIMELOCK) {
+            revert LoopNotActivated();
+        }
 
         // deposits all the ETH to lpETH contract. Receives lpETH back
         lpETH.deposit{value: totalSupply}(address(this));
         totalLpETH = lpETH.balanceOf(address(this));
 
-        // Claims of lpETH can start with one day delay after conversion.
-        startClaimDate = uint32(block.timestamp + 1 days);
+        // Claims of lpETH can start immediately after conversion.
+        startClaimDate = uint32(block.timestamp);
 
         emit Converted(totalSupply, totalLpETH);
     }
@@ -150,21 +208,28 @@ contract PrelaunchPoints {
     /**
      * @notice Sets the lpETH contract address
      * @param _loopAddress address of the lpETH contract
-     * @dev Can only be set once before 120 have passed from deployment. After that users can only withdraw ETH.
+     * @dev Can only be set once before 120 days have passed from deployment.
+     *      After that users can only withdraw ETH.
      */
-    function setLoopAddress(address _loopAddress) external onlyAuthorized onlyBeforeDate(loopActivation){
+    function setLoopAddresses(address _loopAddress, address _vaultAddress)
+        external
+        onlyAuthorized
+        onlyBeforeDate(loopActivation)
+    {
         lpETH = ILpETH(_loopAddress);
+        lpETHVault = ILpETHVault(_vaultAddress);
         loopActivation = uint32(block.timestamp);
 
-        emit LoopAddressUpdated(_loopAddress);
+        emit LoopAddressesUpdated(_loopAddress, _vaultAddress);
     }
-
 
     /**
      * @dev Allows the owner to recover other ERC20s mistakingly sent to this contract
      */
     function recoverERC20(address tokenAddress, uint256 tokenAmount) external onlyAuthorized {
-        require(tokenAddress != address(lpETH), "Not valid token");
+        if (tokenAddress == address(lpETH)) {
+            revert NotValidToken();
+        }
         IERC20(tokenAddress).safeTransfer(owner, tokenAmount);
 
         emit Recovered(tokenAddress, tokenAmount);
@@ -177,20 +242,28 @@ contract PrelaunchPoints {
         revert();
     }
 
-    // ----- Modifiers ----- //
+    /*//////////////////////////////////////////////////////////////
+                                MODIFIERS
+    //////////////////////////////////////////////////////////////*/
 
     modifier onlyAuthorized() {
-        require(msg.sender == owner, "!auth");
+        if (msg.sender != owner) {
+            revert NotAuthorized();
+        }
         _;
     }
 
     modifier onlyAfterDate(uint256 limitDate) {
-        require(block.timestamp > limitDate, "Currently not possible");
+        if (block.timestamp <= limitDate) {
+            revert CurrentlyNotPossible();
+        }
         _;
     }
 
     modifier onlyBeforeDate(uint256 limitDate) {
-        require(block.timestamp < limitDate, "No longer possible");
+        if (block.timestamp >= limitDate) {
+            revert NoLongerPossible();
+        }
         _;
     }
 }
